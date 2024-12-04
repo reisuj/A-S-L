b5a17d5f980501536d9cbc9d48d59b16722d0103
reisuj <reisuj@gmail.com>
2024-12-04 16:43:21 -0500

    Added sex and location variables and added to output when pressing Space key

--- Assets/ASL.cs
+++ Assets/ASL.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class ASL : MonoBehaviour
 {
     [SerializeField] int _age;
+    [SerializeField] string _sex;
+    [SerializeField] string _location;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +17,11 @@ public class ASL : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("You're age is " + _age + ".");
+            //Debug.Log("You're age is " + _age + ".");
+            //Debug.Log("Your sex is " + _sex + ".");
+            //Debug .Log("Your location is " + _location + ".");
+
+            Debug.Log($"My age is {_age}, my sex is {_sex}, and my location is {_location}");
         }
     }
 }
